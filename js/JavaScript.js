0288eeaa021195f420e5e5ac98dccf7e3b254801
JavierSalazar19 <salazarcoronado1909@gmail.com>
2024-10-29 13:57:19 -0700

    Agregar archivos de proyecto.

---
+++ js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿const perroActualElement = document.getElementById("perroActual");
+const API = "https://dog.ceo/api/breeds/image/random";
+
+document.getElementById("siguiente").addEventListener("click", nuevoPerro);
+
+async function nuevoPerro() {
+    perroActualElement.classList.toggle("escondido",true);
+    const res = await fetch(API);
+    const resJson = await res.json();
+    perroActualElement.src = resJson.message;
+    perroAcualElement.classList.toggle("escondido",false);
+}
+
+nuevoPerro();
